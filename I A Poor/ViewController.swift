0a0b2dd563642c580bab2d112d414d3f7c81b95d
Daniel Alves <Danielalvestrab@gmail.com>
2024-10-25 15:21:02 -0300

    Initial Commit

---
+++ I A Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I A Poor
+//
+//  Created by Fiocruz on 25/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
